0ce830e31939f548af7de9ee50e86b7a9f069054
Nicole Martindale <nmartind@ucsd.edu>
2021-02-25 13:18:57 -0800

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'pX1xGCyJCOSTax3Ah3FGIWkaPj2xX2AZ';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	center: [32.87850361694441, -117.23587945682593],
+	layers: L.mapquest.tileLayer('map'),
+	zoom: 12,
+	zoomControl: false
+	});
+
+	L.marker([32.87850361694441, -117.23587945682593]).addTo(map);
 }
